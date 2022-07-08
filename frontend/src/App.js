5ceda6cbdec2a10dcb00c21bfaf58bcb3e1069ca
Womesh Ankit <womeshankit100@gmail.com>
2022-07-08 17:36:02 +0530

    First commit - Skylog project create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <a href="/">SKYLOG</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
