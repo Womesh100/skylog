7e4b0d62405968fe6c84fe535d52203476e8e05d
Womesh Ankit <womeshankit100@gmail.com>
2022-07-09 21:22:05 +0530

    Product Listing in our App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,35 @@
+import data from './data';
+
 function App() {
   return (
     <div className="">
       <header className="">
         <a href="/">SKYLOG</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map(
+            //jsx expession
+            (product) => (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.Image} alt={product.Image} />{' '}
+                </a>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p>
+                    <strong>{product.price}</strong>
+                  </p>
+                  <button>Add to Cart</button>
+                </div>
+              </div>
+            )
+          )}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,58 @@
+// create object to return sample products.
+
+//define data as an object
+const data = {
+  // define product as an array's object
+  products: [
+    {
+      name: 'nike slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'shirts',
+      Image: '/images/p1.jpg', //679px X 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirts',
+    },
+    {
+      name: 'adidas slim shirt',
+      slug: 'adidas-slim-shirt',
+      category: 'shirts',
+      Image: '/images/p2.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirts',
+    },
+    {
+      name: 'nike slim pants',
+      slug: 'nike-slim-pants',
+      category: 'pants',
+      Image: '/images/p3.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality pants',
+    },
+    {
+      name: 'adidas fit pants',
+      slug: 'adidas-fit-pants',
+      category: 'pants',
+      Image: '/images/p4.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality pants',
+    },
+  ],
+};
+
+export default data;
